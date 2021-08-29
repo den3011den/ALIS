e9904d399db4c79b565c3cfa6e8f9062d7e8c8be
den3011den <den3011den@gmail.com>
2021-08-29 15:52:09 +0700

    Доработка добавления ролей и административных пользователей в DbInitializer.

--- ALIS_DataAccess/Initializer/DbInitializer.cs
+++ ALIS_DataAccess/Initializer/DbInitializer.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ALIS_DataAccess.Repository;
 
 namespace ALIS_DataAccess.Initializer
 {
@@ -17,12 +18,14 @@ namespace ALIS_DataAccess.Initializer
         private readonly ApplicationDbContext _db;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IPersonRepository _personRepo;
 
-        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IPersonRepository personRepo)
         {
             _db = db;
             _userManager = userManager;
             _roleManager = roleManager;
+            _personRepo = personRepo;
         }
 
         public void Initialize()
@@ -39,36 +42,77 @@ namespace ALIS_DataAccess.Initializer
 
             }
 
-            bool retFlag = true;
             if (!_roleManager.RoleExistsAsync(WC.AdminRole).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(WC.AdminRole)).GetAwaiter().GetResult();
-                retFlag = false;
             }
             
             if (!_roleManager.RoleExistsAsync(WC.ClientRole).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(WC.ClientRole)).GetAwaiter().GetResult();
             }
+            
 
-            if (retFlag)
-                return;
+            Person person1 = _personRepo.FirstOrDefault(u => u.Name == "Екатерина" && u.Surname == "Сенотрусова" && u.Patronymic == "Сергеевна");
+            Person person2 = _personRepo.FirstOrDefault(u => u.Name == "Михаил" && u.Surname == "Сенотрусов" && u.Patronymic == "Сергеевич");
+            Person person3 = _personRepo.FirstOrDefault(u => u.Name == "Денис" && u.Surname == "Богомолов" && u.Patronymic == "Сергеевич");
 
 
-            //FullName = "Admin Tester",
-                        
-
-            _userManager.CreateAsync(new ApplicationUser
+            if (String.IsNullOrEmpty(person1.UserId))
             {
-                UserName = "admin@gmail.com",
-                Email = "admin@gmail.com",
-                EmailConfirmed = true,
-                FullName = "Admin Tester",
-                PhoneNumber = "111111111",
-            }, "Admin123*").GetAwaiter().GetResult();
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = person1.Email,
+                    Email = person1.Email,
+                    EmailConfirmed = true,
+                    FullName = person1.Surname + " " + person1.Name + " " + person1.Patronymic,
+                    PhoneNumber = person1.PhoneNumber,
+                }, "Admin123*").GetAwaiter().GetResult();
 
-            ApplicationUser user = _db.ApplicationUser.FirstOrDefault(u=>u.Email== "admin@gmail.com");
-            _userManager.AddToRoleAsync(user, WC.AdminRole).GetAwaiter().GetResult();
+                ApplicationUser user1 = _db.ApplicationUser.FirstOrDefault(u => u.Email == person1.Email);
+                _userManager.AddToRoleAsync(user1, WC.AdminRole).GetAwaiter().GetResult();
+
+                person1.UserId = user1.Id;
+                _personRepo.Save();
+            }
+
+            if (String.IsNullOrEmpty(person2.UserId))
+            {
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = person2.Email,
+                    Email = person2.Email,
+                    EmailConfirmed = true,
+                    FullName = person2.Surname + " " + person2.Name + " " + person2.Patronymic,
+                    PhoneNumber = person2.PhoneNumber,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user2 = _db.ApplicationUser.FirstOrDefault(u => u.Email == person2.Email);
+                _userManager.AddToRoleAsync(user2, WC.AdminRole).GetAwaiter().GetResult();
+
+                person2.UserId = user2.Id;
+                _personRepo.Save();
+            }
+
+
+            if (String.IsNullOrEmpty(person3.UserId))
+            {
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = person3.Email,
+                    Email = person3.Email,
+                    EmailConfirmed = true,
+                    FullName = person3.Surname + " " + person3.Name + " " + person3.Patronymic,
+                    PhoneNumber = person3.PhoneNumber,
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user3 = _db.ApplicationUser.FirstOrDefault(u => u.Email == person3.Email);
+                _userManager.AddToRoleAsync(user3, WC.AdminRole).GetAwaiter().GetResult();
+
+                person3.UserId = user3.Id;
+                _personRepo.Save();
+            }
+
         }
     }
 }
