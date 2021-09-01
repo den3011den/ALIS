7b651af57afc5cb2b24d0e87a9a5ce7ab856546c
den3011den <den3011den@gmail.com>
2021-09-02 00:10:29 +0700

    Добавлен список типов авторов.

---
+++ ALIS/wwwroot/js/authortype.js
@@ -0,0 +1,113 @@
+﻿var dataTable;
+
+
+
+$(document).ready(function () {
+    loadDataTable("");
+    $('#myTable').DataTable();
+
+});
+
+function loadDataTable(url) {
+    dataTable = $("#tblData").DataTable({
+
+        "initComplete": 
+            function() {
+            this.api().column(4).every(function () {
+                var column = this;                
+                column.search('false').draw();
+                var select = $('#show_archive_records')             
+                    .on('change', function () {
+                        if (this.checked)                            
+                            column.search('^(true|false)', true, false).draw();
+                        else
+                            column.search(false).draw();
+                    });          
+                });
+            },
+
+        /*"order": [[2, "asc"], [1, 'asc']],*/
+        "order": [1, 'asc'],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
+        },
+        "ajax": {
+            "url": "/authortype/GetAuthorTypeList" + url
+        },
+        "columns": [
+            { "data": "id", "width": "3%" },
+            { "data": "name", "width": "50%" },
+            {
+                "data": "isArchive",
+                "render": function (data) {
+                    if (data === true) {
+                        return `<div style="text-align: center;"><span style="color:red"><i class="fas fa-archive"></i></span></div>`;
+                    }
+                    return data;
+                },
+                "width": "1%"
+            },
+
+            {
+                "data": "id",
+                "render": function (data, type, row, meta) {
+                    if (row.isArchive === true) {
+                        return `
+                        <div style="text-align: center;">
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/AuthorType/Edit/${row.id}" class="btn btn-success text-white" style="cursor:pointer" title="Редактировать запись">
+                                   <i class="fas fa-edit mx-2"></i>
+                                 <a href="/AuthorType/Restore/${row.id}" class="btn btn-warning text-white" style="cursor:pointer" title="Восстановить запись">
+                                     <i class="fas fa-trash-restore-alt mx-2"></i>
+                                 </a>
+                             </div>
+                        </div>
+                    `;
+                    }
+                    return `
+                        <div style="text-align: center;">
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/AuthorType/Edit/${row.id}" class="btn btn-success text-white" style="cursor:pointer" title="Редактировать запись">
+                                   <i class="fas fa-edit mx-2"></i>
+                                 <a href="/AuthorType/Delete/${row.id}" class="btn btn-danger text-white" style="cursor:pointer" title="Удалить запись">
+                                     <i class="fas fa-trash-alt mx-2"></i>
+                                 </a>
+                             </div>
+                        </div>
+                    `;
+
+ 
+                },
+                "width": "3%"
+            },
+            {
+                "data": "isArchive",
+                "render": function (data) {
+                    if (data === true) {
+                        return `true`;
+                    }
+                    return `false`;
+                },
+                "visible": false
+            },
+        ]
+
+    });
+
+    $('#show_archive_records').on('click', function () {
+        if (this.checked) {
+            $.fn.dataTable.ext.search.push(
+                function (settings, data, dataIndex) {
+                    return true;
+                }
+            );
+        }
+
+        dataTable.draw();
+
+        if (this.checked) {
+            $.fn.dataTable.ext.search.pop();
+        }
+    });
+
+}
